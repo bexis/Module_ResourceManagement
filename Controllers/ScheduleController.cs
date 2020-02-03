140dd645da88d7e3c9b2e5268aec393702cd5d73
Franziska Zander <franzi82@gmx.de>
2020-02-03 18:06:15 +0100

    Fix quantity not allowed for exact number

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -744,7 +744,7 @@ namespace BExIS.Modules.RBM.UI.Controllers
 
 
 
-                        if ((availableQuantity - s.ScheduleQuantity) <= 0)
+                        if ((availableQuantity - s.ScheduleQuantity) < 0)
                         {
                             isError = true;
                             sError = true;
